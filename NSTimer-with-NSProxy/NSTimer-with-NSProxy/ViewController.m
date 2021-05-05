53ed977808a3a740b3ea4e5e6690464e925baccd
Huang-Libo <LiboHwang+IT@gmail.com>
2021-05-05 17:40:51 +0800

    Update NSTimer-with-NSProxy

--- NSTimer-with-NSProxy/NSTimer-with-NSProxy/ViewController.m
+++ NSTimer-with-NSProxy/NSTimer-with-NSProxy/ViewController.m
@@ -22,7 +22,15 @@
     
     // 强引用链: self -> timer -> proxy , 而 proxy 弱引用 self, 不会形成循环引用
     LBWeakProxy *proxy = [LBWeakProxy proxyWithTarget:self];
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:proxy selector:@selector(timerTriggered:) userInfo:nil repeats:YES];
+    
+    /// 方式一: 使用默认的 run loop mode (这种情况下, `timer` 属性可直接用 weak 修饰, 因为 run loop 会强引用 `timer`)
+    /*
+     self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:proxy selector:@selector(timerTriggered:) userInfo:nil repeats:YES];
+     */
+    
+    /// 方式二: 自行指定 run loop mode
+    self.timer = [NSTimer timerWithTimeInterval:1 target:proxy selector:@selector(timerTriggered:) userInfo:nil repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
 }
 
 - (void)timerTriggered:(NSTimer *)timer {
